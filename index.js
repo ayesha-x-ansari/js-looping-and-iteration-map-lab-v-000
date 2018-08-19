870e945c05912996d44a216e9607a0e5682d436d
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-08-19 20:12:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution in this file.
+
+function  lowerCaseDrivers(drivers) {function (drivers) {
+  let lowerCaseDriversName  = []
+  return drivers.map(driversName){return driversName.toLowerCase()} }
+}
